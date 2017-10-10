106c593481ec05aeccd8ef7ba4e952d33733c49c
murpman300 <jeanlouismurphy@gmail.com>
2017-10-10 14:39:41 -0400

    Update

--- Booq/SpotitColors.swift
+++ Booq/SpotitColors.swift
@@ -20,13 +20,11 @@ struct SpotitColors {
         static let responseRedDarker : UIColor = UIColor.rgb(red: 136, green: 0, blue: 9)
     }
     
-    
     static let tungsten : UIColor = UIColor.rgb(red: 51, green: 51, blue: 51)
     static let iron : UIColor = UIColor.rgb(red: 102, green: 102, blue: 102)
     static let silver : UIColor = UIColor.rgb(red: 204, green: 204, blue: 204)
     static let mercury : UIColor = UIColor.rgb(red: 230, green: 230, blue: 230)
     static let creamy : UIColor = UIColor.rgb(red: 240, green: 240, blue: 240)
     static let dairy : UIColor = UIColor.rgb(red: 250, green: 250, blue: 250)
-    
     
 }
